990e24967155b56624155b08058b6f26569f2e7b
Hussein Kira <husseinkira1998@gmail.com>
2022-12-26 12:22:39 +0200

    exercise 3.9

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 const morgan = require('morgan')
+app.use(express.static('build'))
 
 
 const m =function (tokens, request, response) {
